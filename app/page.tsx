3228a3664ec547f2c01ec82f12c974234c8a5aa1
raj kumar <razzkumar115@gmail.com>
2025-01-28 14:53:01 +0545

    added floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
    <div>
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-2-7xl w-full">
+        <FloatingNav navItems={[
+          {name: 'Home', link : '/', icon:<FaHome/>}
+        ]}/>
         <Hero/>
 
         <h1>hello</h1>
